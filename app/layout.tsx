3ec4d212b4928c5c24fc7a743b73c6d098a20380
Zac Denham <zdenham1@gmail.com>
2026-03-28 14:59:50 -0700

    initial web scaffold

---
+++ app/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "faam",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
